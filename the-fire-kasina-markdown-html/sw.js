a6f409d85f06fa283aae73e5901b56d404b35561
maxrostov <maxrostov@users.noreply.github.com>
2026-05-24 23:22:22 +0300

    favicon, refactor

--- the-fire-kasina-markdown-html/sw.js
+++ the-fire-kasina-markdown-html/sw.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = "c631f287c35a3fa8";
+const BUILD_VERSION = "125e9626a3fbc29f";
 const CACHE_PREFIX = "kasina-publication-";
 const MANIFEST_URL = new URL("offline-manifest.json", self.location.href);
 
